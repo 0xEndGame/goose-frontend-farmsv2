39b9cf82a828f169fdb918c6ea38ab4090ef42f3
Merkel <you@example.com>
2022-02-22 10:35:54 +0100

    da

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -42,9 +42,9 @@ const StyledCardAccent = styled.div`
   rgba(186, 12, 248, 1) 80%,
   rgba(251, 7, 217, 1) 90%,
   rgba(255, 0, 0, 1) 100%);
-  background-size: 300% 300%;
+  background-size: 100% 100%;
   animation: ${RainbowLight} 2s linear infinite;
-  border-radius: 16px;
+  border-radius: 6px;
   filter: blur(6px);
   position: absolute;
   top: -2px;
@@ -67,20 +67,6 @@ const FCard = styled.div`
 
 const Divider = styled.div`
   background-color: ${({ theme }) => theme.colors.borderColor};
-  background: linear-gradient(45deg,
-    rgba(255, 0, 0, 1) 0%,
-    rgba(255, 154, 0, 1) 10%,
-    rgba(208, 222, 33, 1) 20%,
-    rgba(79, 220, 74, 1) 30%,
-    rgba(63, 218, 216, 1) 40%,
-    rgba(47, 201, 226, 1) 50%,
-    rgba(28, 127, 238, 1) 60%,
-    rgba(95, 21, 242, 1) 70%,
-    rgba(186, 12, 248, 1) 80%,
-    rgba(251, 7, 217, 1) 90%,
-    rgba(255, 0, 0, 1) 100%);
-    background-size: 300% 300%;
-    animation: ${RainbowLight} 2s linear infinite;
   height: 1px;
   margin: 28px auto;
   width: 100%;
